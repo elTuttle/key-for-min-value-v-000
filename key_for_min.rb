3d3027e8867d2bcd818c4efda8b5f3868371cb93
John Tuttle <thejwt@gmail.com>
2017-09-26 19:35:19 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   name_array = name_hash.collect do |key, element|
     element
   end
-  
+
   puts name_array
   name_array = name_array.sort
   puts name_array
@@ -14,8 +14,9 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, element|
     if element == name_array[0]
       return key
+    end
   end
-  
+
 end
 
 name_hash_temp = {"john1" => 20, "john2" => 10}
